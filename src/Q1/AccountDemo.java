b622b4b37f03b0cb016b5a8e91818a64ff57fa05
Ohad hasson <50565483+ohad1992@users.noreply.github.com>
2022-03-21 11:06:08 +0200

    Update AccountDemo.java

--- src/Q1/AccountDemo.java
+++ src/Q1/AccountDemo.java
@@ -9,12 +9,14 @@ public class AccountDemo {
     public static void main(String[] args) {
 
         HashMap<Integer, Account> accounts = new HashMap<>();
+        // initilize 4 accounts with account num from 0-5 and balance=0
         for (int i = 0; i < 5; i++) {
             Account a = new Account(i, 0);
             accounts.put(i, a);
         }
 
         ArrayList<Transaction> transactions = new ArrayList<>();
+        // create resource of movements with random numbers for deposit and withdraw
         for(int i = 0; i < 5; i++) {
             for (int j = 0; j < 4; j++)
                 transactions.add(new Transaction(i, new Random().nextInt(500) - 300));
@@ -22,6 +24,7 @@ public class AccountDemo {
         Transactions t = new Transactions(transactions);
 
         BankAdminstration[] admins = new BankAdminstration[10];
+        // create 10 procceses
         for (BankAdminstration admin : admins) {
             admin = new BankAdminstration(t, accounts);
             admin.start();
